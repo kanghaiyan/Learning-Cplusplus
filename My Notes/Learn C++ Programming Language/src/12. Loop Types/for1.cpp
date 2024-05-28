a7e623524287398e510dd70a32f798cacb794134
Olem Diga <167544383+olemdiga@users.noreply.github.com>
2024-05-28 14:33:18 +0200

    Add files via upload

---
+++ My Notes/Learn C++ Programming Language/src/12. Loop Types/for1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main ()
+{
+    // for loop execution
+    for( int a = 10; a < 20; a = a + 1 )
+    {
+        std::cout << "value of a: " << a << std::endl;
+    }
+
+    return 0;
+}
---
+++ My Notes/Learn C++ Programming Language/src/12. Loop Types/while1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main ()
+{
+    // Local variable declaration:
+    int a = 10;
+
+    // while loop execution
+    while( a < 20 )
+    {
+        std::cout << "value of a: " << a << std::endl;
+        a++;
+    }
+
+    return 0;
+}
