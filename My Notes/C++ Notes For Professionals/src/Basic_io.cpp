527e33494e4e450d91ed4f4acb90eb9341263047
Olem Diga <167544383+olemdiga@users.noreply.github.com>
2024-06-05 21:39:41 +0200

    Add files via upload

---
+++ My Notes/C++ Notes For Professionals/src/Basic_io.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main(){
+    int value;
+    std::cout << "Enter a value: " <<std::endl;
+    std::cin >> value;
+    std::cout << "The square of entered value is: " << value * value << std::endl;
+    return 0;
+}
